0f98930e53a983b8ad23658532220f59d78516df
Levin Keller <post@levinkeller.de>
2024-06-26 09:21:45 +0000

    chore: refactor code to be more telling

--- src/liveBalance.test.ts
+++ src/liveBalance.test.ts
@@ -3,37 +3,47 @@ import { describe, test, expect, vi, onTestFinished } from "vitest";
 import { liveBalance } from "./liveBalance";
 import bip39 from "bip39";
 
-const lcd = new LCDClient("http://localhost:1317", {
+describe("Live user balance", () => {
+  test("The balance updates when funds are received", async () => {
+    // arrange
+    const randomWallet = createRandomWallet();
+    const balance = await liveBalance({
+      websocketUrl: "ws://localhost:26657/websocket",
+      lcdUrl: "http://localhost:1317",
+    })(randomWallet.key.accAddress);
+
+    onTestFinished(balance.destroy);
+
+    // assert
+    expect(balance.getBalance()).toBe(0);
+
+    await waitForBoth([
+      // act
+      sendFunds(randomWallet.key.accAddress, 1000),
+      // assert
+      vi.waitFor(() => expect(balance.getBalance()).toBe(1000)),
+    ]);
+  });
+});
+
+
+const localLCDClient = new LCDClient("http://localhost:1317", {
   gasPrices: "0.15umin",
   gasAdjustment: "1.5",
 });
 const key = new MnemonicKey({ mnemonic: process.env.VITE_MNEMONIC });
-const wallet = new Wallet(lcd, key);
+const wallet = new Wallet(localLCDClient, key);
 
-describe("Live user balance", () => {
-  test("The balance updates when funds are received", async () => {
-    const randomKey = new MnemonicKey({ mnemonic: bip39.generateMnemonic() });
-    const randomWallet = new Wallet(lcd, randomKey);
-    const balance = await liveBalance({
-      websocketUrl: "ws://127.0.0.1:26657/websocket",
-      lcdUrl: "http://localhost:1317",
-    })(randomWallet.key.accAddress);
-    onTestFinished(balance.destroy);
-
-    expect(balance.getBalance()).toBe(0);
-
-    await Promise.all([
-      sendFunds(randomWallet.key.accAddress, 1000),
-      vi.waitFor(() => expect(balance.getBalance()).toBe(1000), {
-        timeout: 5000,
-      }),
-    ]);
-  }, 6000);
-});
 
 const sendFunds = async (recipient: string, amount: number) =>
   wallet
     .createAndSignTx({
       msgs: [new MsgSend(wallet.key.accAddress, recipient, `${amount}umin`)],
     })
-    .then(lcd.tx.broadcast.bind(lcd.tx));
+    .then(localLCDClient.tx.broadcast.bind(localLCDClient.tx));
+
+const createRandomWallet = () => 
+ new Wallet(localLCDClient, new MnemonicKey({ mnemonic: bip39.generateMnemonic() }));
+
+
+const waitForBoth = Promise.all.bind(Promise);
